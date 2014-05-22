4961abe0ccb1ddfacf93a4522d13c13f9172ca31
alexeiTruhin <alexei.truhin@gmail.com>
2014-05-22 15:47:58 +0200

    added mongoDB to app

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,4 +6,53 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('user');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "users" : docs
+        });
+    });
+});
+
+/* Post a user in Userlist. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var userEmail = req.body.useremail;
+    var userPermission = req.body.userpermission;
+
+    // Set our collection
+    var collection = db.get('user');
+
+    // Submit to the DB
+    collection.insert({
+        "username" : userName,
+        "email" : userEmail,
+        "permission" : userPermission
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
